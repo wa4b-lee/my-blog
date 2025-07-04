26a960707afa49237b25da764f04d5913d6de729
wa4b <wocjfl12@naver.com>
2025-07-05 06:34:30 +0900

    테스트입니다

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>다시 시작 해봅니다. 될때까지 합왜리허세 다.  
+  <div>다시 진짜 이번에 성공 하길 바래요. 
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
