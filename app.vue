ea0a80b4bae3b63440a15ec57715aed00db667f1
wa4b <wocjfl12@naver.com>
2025-07-05 09:51:16 +0900

    감동입니다1.

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>!!너무 감동이다 다시 진짜 이번에 성공 하길 바래요. 깃 마스터 할꺼에요wueuoiuwoieuowiueoiwueouwoeuwoeuoweuoiw 
+  <div>감자야 사랑해~~
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
