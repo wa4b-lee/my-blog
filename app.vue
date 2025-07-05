84b55b7eb2e4ea5f60e44c0691da147e7a63a5ec
wa4b <wocjfl12@naver.com>
2025-07-05 10:30:03 +0900

    젠킨스 부시자

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>000감자야 사랑해 진짜로~~~~!!!!!!!!!!!!!!!!!!!!!!!~~
+  <div>젠킨스 너 부셔버릴꺼야!!
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
