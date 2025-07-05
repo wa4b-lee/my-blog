ff40807be32a22ef13ef5cead7a027fee0420d25
wa4b <wocjfl12@naver.com>
2025-07-05 10:10:28 +0900

    감동입니다322222223111.

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>!!!!!!!!!!!!!!ㅈㅈㅈㅈㄷㅈㄷㅈㄷㅈㄷㅈㄷㅈㄷㅈㄷ!!!!!감자야 사랑해 진짜로~~~~!!!!!!!!!!!!!!!!!!!!!!!~~
+  <div>000감자야 사랑해 진짜로~~~~!!!!!!!!!!!!!!!!!!!!!!!~~
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
