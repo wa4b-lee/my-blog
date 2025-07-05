3dffe93a563ee72ed7c34dbf806ab87741bc2284
wa4b <wocjfl12@naver.com>
2025-07-06 05:34:11 +0900

    20250706 테스트

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>오늘은 젠킨스 박살을 냈습니다. 역사적인 날입니다.
+  <div>오늘은 젠킨스 박살을 냈습니다. 역사적인 날입니다. 테스트 합니다.
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
