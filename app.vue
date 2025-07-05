6a2baff8f99eafd7f2f361bff839ea8535000077
wa4b <wocjfl12@naver.com>
2025-07-05 10:33:18 +0900

    젠킨스 개작살 내버려

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>젠킨스 너 부셔버릴꺼야!!
+  <div>젠킨스 너 부셔버릴꺼야!! 초전박살 개박살 너 죽었어
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
