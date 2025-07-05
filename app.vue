79470af945663f3a87e37081150894cbb0d73398
wa4b <wocjfl12@naver.com>
2025-07-05 09:37:45 +0900

    수정중111입니다.

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>!!다시 진짜 이번에 성공 하길 바래요. 깃 마스터 할꺼에요 
+  <div>!!다시 진짜 이번에 성공 하길 바래요. 깃 마스터 할꺼에요wueuwiueuwoeuwueuweouwioueowueoiuwoieuowiueoiwueouwoeuwoeuoweuoiw 
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
