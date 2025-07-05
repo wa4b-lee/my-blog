957fa51f141ea35272d50055767ea1c39e58f222
wa4b <wocjfl12@naver.com>
2025-07-05 10:46:06 +0900

    젠킨스 개작살 내버려11

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>젠킨스 너 부셔버릴꺼야!! 초전박살 개박살 너 죽었어
+  <div>오늘은 젠킨스 박살을 냈습니다. 역사적인 날입니다.
       <img src="https://wicecream-images.s3.ap-northeast-2.amazonaws.com/thumbnails/test.png" alt=""/>
   </div>
 </template>
